df0d8684f0f4ac447ebbc1e1d67352b738289bf3
Agustin <agusstingalvan2@gmail.com>
2026-04-22 18:37:02 -0300

    ejercicio 1

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log("Guía JavaScript iniciada");
+
+const nombre = "Luna";
+let edad = 18;
+let vida = 100;
+let puntaje = 0;
+let estaActivo = true;
+
+console.log(nombre, edad, vida, puntaje, estaActivo);
